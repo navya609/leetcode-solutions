9af0e98cf27a7734a4c6a0d5a65a9e79e0c57cea
navya609 <navyayaganti33@gmail.com>
2026-05-29 14:38:42 +0530

    Time: 0 ms (100%), Space: 12.4 MB (21.19%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,20 +1,21 @@
 class Solution:
     def merge(self, nums1, m, nums2, n):
 
-        i = m - 1  # last valid element in nums1
-        j = n - 1  # last element in nums2
-        k = m + n - 1  # fill position in nums1
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
 
         while i >= 0 and j >= 0:
+
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
                 i -= 1
             else:
                 nums1[k] = nums2[j]
                 j -= 1
+
             k -= 1
 
-        # if nums2 still has elements
         while j >= 0:
             nums1[k] = nums2[j]
             j -= 1
